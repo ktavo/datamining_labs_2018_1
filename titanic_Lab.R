04bb6531d2dae5666a584bfbe4c8d9deacfb3ffd
Tavo <gariosp@unal.edu.co>
2018-06-05 20:21:57 -0300

    Adding basic probabilities

--- titanic_Lab.R
+++ titanic_Lab.R
@@ -7,3 +7,10 @@ load("titanic.raw.rdata")
 
 # ¿Cuál es el soporte de Survived=Yes?
 nrow(titanic.raw[Survived == 'Yes',])/nrow(titanic.raw)
+
+# ¿Cuál es el soporte de Survived=Yes y Sex=Male?
+nrow(titanic.raw[Survived == 'Yes' & Sex == 'Male' ,])/nrow(titanic.raw)
+
+#¿Cuales itemsets de los que se listan a continuación tienen mayor soporte?
+nrow(titanic.raw[Survived == 'Yes' & Sex == 'Male' & Class == '3rd',])/nrow(titanic.raw) #0.03998183
+nrow(titanic.raw[Survived == 'No' & Sex == 'Male' & Class == '3rd',])/nrow(titanic.raw) #0.191731
